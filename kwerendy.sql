f4f2407d27a387ed5d2052be8ff895e07d473ca6
pan7nikt <43100201+pan7nikt@users.noreply.github.com>
2022-11-14 12:32:28 +0100

    Add files via upload

--- kwerendy.sql
+++ kwerendy.sql
@@ -1,6 +1,6 @@
 SELECT `nazwa`,`cena`,`dostepnosc` FROM `podzespoly` WHERE `producenci_id`=5
 
-SELECT `id`,`opis` FROM `podzespoly` WHERE `dostepnosc`= 0 AND `producenci_id`=2;
+SELECT `id`,`opis` FROM `podzespoly` WHERE `dostepnosc`= 0 AND `producenci_id`=2 OR 1;
 
 SELECT podzespoly.opis,podzespoly.cena,producenci.nazwa FROM `podzespoly` JOIN `producenci` ON podzespoly.producenci_id = producenci.id;
 
